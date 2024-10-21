39c420959b4eba918df1fc03710b5dce5bd6192d
张施泽 <1194067216@qq.com>
2024-10-21 17:57:29 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    mocha: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
